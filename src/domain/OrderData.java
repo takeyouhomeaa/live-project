07a0160d007025dd75ef0a239c9a50be24b33bfa
1209674748 <1209674748@qq.com>
2020-03-15 16:00:19 +0800

    orderdata数据接口

---
+++ src/domain/OrderData.java
@@ -0,0 +1,42 @@
+package domain;
+
+import java.sql.Date;
+
+public class OrderData {
+	private Date start;
+	private Date end;
+	private int sum;
+	
+	public OrderData() {
+		// TODO Auto-generated constructor stub
+	}
+	public OrderData(Date start,Date end,int sum)
+	{
+		this.start=start;
+		this.end=end;
+		this.sum=sum;
+	}
+	
+	public void setStart(Date start)
+	{
+		this.start=start;
+	}
+	public Date getStart()
+	{
+		return start;
+	}
+	
+	public void setEnd(Date end) {
+		this.end=end;
+	}
+	public Date getEnd() {
+		return end;
+	}
+	
+	public void setSum(int sum) {
+		this.sum=sum;
+	}
+	public int getSum() {
+		return sum;
+	}
+}
